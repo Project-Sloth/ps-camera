3097930275a48a92f44367bc11b5ca94247f5ef8
Lenzh <lorenzo0109@hotmail.nl>
2023-05-08 01:20:12 +0200

    Adds Inventory support and webhook to conf

--- config.lua
+++ config.lua
@@ -1,1 +1,4 @@
 Config = Config or {}
+
+Config.Inv = "ox" -- qb(=lj) or ox [Inventory system]
+Config.webhook = "https://discord.com/api/webhooks/1098347143602913340/61nQVpgAZILNJyUNN3v6AP2yvtR8VLKcOjNVcfGYpKBfotrpqNy4uDxsQA-ERh4bqnFi" -- Add Discord webhook
