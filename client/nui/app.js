9f6dcc812e7dfc41b95631a47cf9ed8e31d33673
xFutte <Futte8D@hotmail.com>
2023-06-10 17:00:27 +0200

    Refactored clipboard functionality

--- client/nui/app.js
+++ client/nui/app.js
@@ -1,34 +1,36 @@
 var displayPicture = false;
 
 function setLocation(location) {
-    if (typeof location === "string") {
-        document.getElementById("location").innerHTML = location;
-    } else if (typeof location === "object") {
-        let formattedLocation = `X: ${location.x.toFixed(2)}, Y: ${location.y.toFixed(2)}, Z: ${location.z.toFixed(2)}`;
-        document.getElementById("location").innerHTML = formattedLocation;
-    }
+	if (typeof location === 'string') {
+		document.getElementById('location').innerHTML = location;
+	} else if (typeof location === 'object') {
+		let formattedLocation = `X: ${location.x.toFixed(
+			2
+		)}, Y: ${location.y.toFixed(2)}, Z: ${location.z.toFixed(2)}`;
+		document.getElementById('location').innerHTML = formattedLocation;
+	}
 }
 
 function open(image, location) {
-    if (!displayPicture) {
-        $('.picture-container').removeClass('hide');
-        if (image) {
-            $('.picture').css({
-                'background-image': `url(${image})`,
-            });
-        } else {
-            $('.picture').css({
-                'background-image': `url(https://slang.net/img/slang/lg/kekl_6395.png)`,
-            });
-        }
-        
-        if (typeof location === "string") {
-            $('#location').text(location);
-        } else {
-            setLocation(location);
-        }
-        displayPicture = true;
-    }
+	if (!displayPicture) {
+		$('.picture-container').removeClass('hide');
+		if (image) {
+			$('.picture').css({
+				'background-image': `url(${image})`,
+			});
+		} else {
+			$('.picture').css({
+				'background-image': `url(https://slang.net/img/slang/lg/kekl_6395.png)`,
+			});
+		}
+
+		if (typeof location === 'string') {
+			$('#location').text(location);
+		} else {
+			setLocation(location);
+		}
+		displayPicture = true;
+	}
 }
 
 function close() {
@@ -41,31 +43,22 @@ function close() {
 	}
 }
 
-const copyToClipboard = (str) => {
-  const el = document.createElement("textarea");
-  el.value = str.replace(/[" ]/g, "");
-  document.body.appendChild(el);
-  el.select();
-  document.execCommand("copy");
-  document.body.removeChild(el);
-};
-
 $(document).ready(function () {
-	window.addEventListener("message", function (event) {  
-	  if (event.data.action === "Open") {
-		open(event.data.image, event.data.location);
-		document.getElementById("camera-overlay").classList.remove("hide");
-	  } else if (event.data.action === "SetLocation") {
-		setLocation(event.data.location);
-	  } else if (event.data.action === "showOverlay") {
-		document.getElementById("camera-overlay").classList.remove("hide");
-	  } else if (event.data.action === "hideOverlay") {
-		document.getElementById("camera-overlay").classList.add("hide");
-	  } else if (event.data.action === "openPhoto") {
-		open(event.data.image, event.data.location);
-	  } else if (event.data.action === "SavePic") {
-		copyToClipboard(event.data.pic);
-	  }
+	window.addEventListener('message', function (event) {
+		if (event.data.action === 'Open') {
+			open(event.data.image, event.data.location);
+			document.getElementById('camera-overlay').classList.remove('hide');
+		} else if (event.data.action === 'SetLocation') {
+			setLocation(event.data.location);
+		} else if (event.data.action === 'showOverlay') {
+			document.getElementById('camera-overlay').classList.remove('hide');
+		} else if (event.data.action === 'hideOverlay') {
+			document.getElementById('camera-overlay').classList.add('hide');
+		} else if (event.data.action === 'openPhoto') {
+			open(event.data.image, event.data.location);
+		} else if (event.data.action === 'SavePic') {
+			navigator.clipboard.writeText(str);
+		}
 	});
 
 	document.onkeydown = function (event) {
