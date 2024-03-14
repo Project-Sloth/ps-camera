25e85074a989aef57b9885707bc722ab48918fbe
suryabhaiin <54040750+suryabhaiin@users.noreply.github.com>
2024-03-15 05:00:17 +0530

    Clipboard Copy URL fix
    Clipboard Copy URL fix

--- client/nui/app.js
+++ client/nui/app.js
@@ -43,6 +43,23 @@ function close() {
 	}
 }
 
+function copyToClipboard(text) {
+	text = text.replace(/"/g, '');
+	text = text.trim();
+	var textarea = document.createElement('textarea');
+	textarea.value = text;
+	textarea.style.position = 'absolute';
+	textarea.style.left = '-9999px';
+	document.body.appendChild(textarea);
+	textarea.select();
+	try {
+	  document.execCommand('copy');
+	} catch (err) {
+	  console.error('Unable to copy to clipboard:', err);
+	}
+	document.body.removeChild(textarea);
+}
+
 function toggleflash(status)
 {
 	$('#flashstatus').html(status);
@@ -62,7 +79,7 @@ $(document).ready(function () {
 		} else if (event.data.action === 'openPhoto') {
 			open(event.data.image, event.data.location);
 		} else if (event.data.action === 'SavePic') {
-			navigator.clipboard.writeText(str);
+			copyToClipboard(event.data.pic);
 		}
 		else if (event.data.action === 'toggleFlash') {
 			toggleflash(event.data.status);
